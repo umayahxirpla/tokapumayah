38b54543755e60b7493e3ff3532b5db3d229d39c
umayahxirpla <umayahmay0806@gmail.com>
2019-02-12 08:23:55 -0800

    tokap

---
+++ app/Http/Controllers/userSettingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+// ihfuigygjbjksggy
+
+class userSettingController extends Controller
+{
+    public function form()
+    {
+    	return view('admin.pages.user.setting');
+    }
+}

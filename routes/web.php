5b74be3a9cbe4056a33d04c0180212cde18d3ccd
umayahxirpla <umayahmay0806@gmail.com>
2019-02-16 22:26:53 -0800

    tokap

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::group(['middleware'=>['auth']], function(){
 		})->name('admin.home');
 
 		Route::prefix('user')->group(function(){
+			Route::get('/','UserController@daftar')->name('admin.user');
 			Route::get('/setting','userSettingController@form')->name('admin.user.setting');
 			Route::post('/setting','userSettingController@update');
 		});
